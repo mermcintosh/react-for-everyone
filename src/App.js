4f231bf12c1e9ed95b0a8cf72e1a5df23954fc75
Meredith <meredith@mcintosh.io>
2021-04-12 14:14:09 -0400

    beginning of react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 // import PropTypes from 'prop-types';
 // import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 import {MoviesList} from './movies/MoviesList'
+import {MovieDetail} from './movies/MovieDetail'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 
 
 
 function App() {
   return (
     <div className="App">
-      <MoviesList/>
+      <Router>
+        <Switch>
+          <Route path="/details">
+            <MovieDetail/>
+          </Route>
+          <Route path="/">
+            <MoviesList/>
+          </Route>
+       </Switch>
+      </Router>
     </div>
   );
 }
--- src/movies/Movie.js
+++ src/movies/Movie.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
+import {Link} from "react-router-dom"
 
 
 export function Movie({movie, config}) {
@@ -10,12 +11,14 @@ export function Movie({movie, config}) {
 
     return(
         <li>
+            <Link to={'/movie/${movie.id}'}>
             {config.images?.base_url && (
             <img 
                 src={config.images.base_url + "w342" + movie.poster_path} 
                 alt={movie.title + "Poster"}/>
             )}
             <h3>{movie.title}</h3>
+            </Link>
         </li>
     )
 }
